5067b7b45381eed9e1edea0fe89b2e771d622c6f
Pablo Munoz Hidalgo <53487847+Litecore50@users.noreply.github.com>
2022-09-19 20:51:27 -0600

    GetOps de terminal y resultado en el txt

--- simplex.py
+++ simplex.py
@@ -1,23 +1,43 @@
+from asyncore import write
+from mailbox import NoSuchMailboxError
 import numpy as np
+import sys
+import os
+
+# Esta funcion se encargará de eliminar el archivo txt para borrar todo su contenido
+def removeFile():
+    try:
+        os.remove("resultado.txt")
+    except OSError:
+        pass
 
 # Este será un lector de archivos txt
-def read_file():
-    reader = open("operacion.txt", 'r')
+def readFile(nombre_archivo):
+    reader = open(nombre_archivo, 'r')
     file = reader.read()
     reader.close()
     return file
 
+# Esta funcion se encargar de escribir en el archivo txt
+def writeFile(text):
+    reader = open("resultado.txt", 'a')
+    if type(text) == type(list):
+        for i in text:
+            #reader.write("\n")
+            i = str(i)
+            reader.write(i)
 
-# Este será un escritor de archivos txt
-def write_file(text):
-    reader = open("operacion.txt", 'w')
-    reader.write(text)
+    else:
+        text = str(text)
+        reader.write("\n")
+        reader.write(text)
     reader.close()
 
 
+
 # Esta función se encargará de separar los datos del archivo txt
-def separar_datos():
-    datos = read_file()
+def separarDatos(nombre_archivo):
+    datos = readFile(nombre_archivo)
     datos = datos.split()
     datos2 = []
     for i in range(len(datos)):
@@ -68,16 +88,24 @@ def agregar_variables_augura_restricciones(lista, cantidadRestricciones):
 def calculaCasilla(casilla,pivote):
     casillaNegada = float(float(casilla) * -1.0).__format__()
     resultado = (float(casilla).__format__('0.4f') + casillaNegada) * float(pivote).__format__('0.4f')
-    print("------Casilla: ", casilla, "Pivote: ", pivote, "Resultado: ", resultado,"------")
+    #print("------Casilla: ", casilla, "Pivote: ", pivote, "Resultado: ", resultado,"------")
+    writeFile("------Casilla: " + str(casilla) + "Pivote: " + str(pivote) + "Resultado: " + str(resultado) + "------")
     return resultado
 
 
 def metodoSimplex(problema):
     bandera = True
+    iteracion = 0
+    spacer = "\n-----------------------------------------------------------------------------\n"
+
     while bandera:
-        print("---------------------------------------------------------------")
-        print("-----------------------------Iteracion----------------------------------")
-        print("---------------------------------------------------------------")
+        numero_iteracion = "-----------------------------Iteracion numero: " + str(iteracion) + "-----------------------------"
+        #print(spacer)
+        #print(numero_iteracion)
+        #print(spacer)
+        writeFile(spacer)
+        writeFile(numero_iteracion)
+        writeFile(spacer)
         bandera , colMenor = problema.__indexColumnaMenor__()
         pivote = problema.__determinacionPivote__(colMenor)
         nuevaFila = problema.__nuevaFila__(pivote)#problemas
@@ -85,6 +113,7 @@ def metodoSimplex(problema):
         tablaNueva = problema.__tablaNueva__(nuevaFila, pivote)
         tablaNueva = problema.__simplexCalculo__(pivote, nuevaFila)
         solucionInicial = problema.__solucionInicial__()
+        iteracion += 1
 
 
 
@@ -123,12 +152,15 @@ class problema:
         for i in range(len(self.restricciones)):
             orden.append(i + 1 + int(self.cant_v_decision))
         self.ordenFilas = orden
-        print("Orden de las filas: ", self.ordenFilas)
+        #print("Orden de las filas: ", self.ordenFilas)
+        writeFile("\nOrden de las filas: ")
+        writeFile(self.ordenFilas)
         return self
 
     def __updateOrdenFilas__(self, pivote):
         self.ordenFilas[pivote[1][0]] = int(pivote[1][1])+1
-        print("Orden de las filas: ", self.ordenFilas)
+        #print("Orden de las filas: ", self.ordenFilas)
+        writeFile("\nOrden de las filas: " + str(self.ordenFilas))
 
     def __getRestricciones__(self):
         restricciones = []
@@ -139,7 +171,7 @@ class problema:
         self.restricciones = restricciones
         return self.restricciones
 
-    def __agregar_variables_simplex__(self):
+    def __agregarVariablesSimplex__(self):
         self.objetivo = agregar_variables_augura_objetivo(self.funcion_objetivo, int(self.cant_restricciones))
         self.objetivo = setNumeros(self.objetivo)
         self.restricciones = agregar_variables_augura_restricciones(self.restricciones, int(self.cant_restricciones))
@@ -169,15 +201,18 @@ class problema:
         else:
             tabla = self.tablaSiguiente
         for i in range(len(tabla)):
-            print(tabla[i])
+            writeFile(tabla[i])
+            #print(tabla[i])
 
     def __print__(self):
-        print("Metodo: " + self.metodo + "\n" +
+        informacionProblemaText = ("\nMetodo: " + self.metodo + "\n" +
               "Optimizacion: " + self.optimizacion + "\n" +
               "Cantidad de variables de decision: " + self.cant_v_decision + "\n" +
               "Cantidad de restricciones: " + self.cant_restricciones + "\n" +
               "Funcion objetivo: " + str(self.funcion_objetivo) + "\n" +
               "Restricciones: " + str(self.restricciones))
+        #print(informacionProblemaText)
+        writeFile(informacionProblemaText)
 
     #arreglar para trabajar ordenFilas
     def __solucionInicial__(self):
@@ -188,7 +223,8 @@ class problema:
         resultados = []
         for i in range(1, len(self.tablaActual)):
             resultados.append(self.tablaActual[i][-1])
-        print("Resultados: ", resultados)
+        #print("Resultados: ", resultados)
+        writeFile("\nResultados: " + str(resultados))
         for i in range(len(self.funcion_objetivo)-2):
             auxiliar.append(float(0).__format__('0.4f'))
         for j in range(len(auxiliar)+1):
@@ -196,7 +232,8 @@ class problema:
                 if j == k:
                     auxiliar[j-1] = resultados[self.ordenFilas.index(k)-1]
         solucion.append(auxiliar)
-        print("Solucion inicial: ", solucion)
+        #print("Solucion inicial: ", solucion)
+        writeFile("\nSolucion inicial: " + str(solucion))
         return solucion
 
     def __indexColumnaMenor__(self):
@@ -210,7 +247,8 @@ class problema:
             return False , 0
         else:
             resultado = max(negativos)
-            print("minimo: " , float(self.tablaActual[0].index(resultado)).__format__('0.4f') , " valor: " , float(resultado).__format__('0.4f'))
+            #print("Minimo: " , float(self.tablaActual[0].index(resultado)).__format__('0.4f') , " Valor: " , float(resultado).__format__('0.4f'))
+            writeFile("Minimo: " + str(float(self.tablaActual[0].index(resultado)).__format__('0.4f')) + " Valor: " + str(float(resultado).__format__('0.4f')))
             return True , int(self.tablaActual[0].index(resultado))
 
     def __determinacionPivote__(self, columna):
@@ -237,7 +275,9 @@ class problema:
         for i in range(len(tabla[fila])):
             nuevaFila.append(float(
                 float(tabla[int(fila)][i]) / float(tabla[int(fila)][columna])).__format__('0.4f'))
-        print("Nueva fila: ", nuevaFila)
+        #print("Nueva fila: ", nuevaFila)
+        writeFile("Nueva Fila: ")
+        writeFile(nuevaFila)
         return nuevaFila
 
     def __tablaNueva__(self, nuevaFila, pivote):
@@ -256,15 +296,20 @@ class problema:
     #Esta función, no sirve necesita reparación solo escribe taxto no listas
     def __grabarTabla__(self):
         tabla = self.tablaActual
-        write_file(tabla)
+        writeFile(tabla)
 
     def __simplexCalculo__(self, pivote, nuevaFila):
         tablaActual = self.tablaActual
         tablaNueva = self.tablaSiguiente
-        print("----------------------------------Tabla Actual------------------------------------")
+        tablaActualText = "\n--------------------------------Tabla Actual---------------------------------"
+        writeFile(tablaActualText)
+        #print(tablaActualText)
         self.__printTabla__(1)
-        print("Pivote: " + str(pivote))
-        print("------------------------------Tabla nueva - ceros ---------------------------------")
+        writeFile("\nPivote: " + str(pivote))
+        #print("\nPivote: " + str(pivote))
+        tablaCerosText = "\n---------------------------Tabla nueva - ceros ------------------------------"
+        writeFile(tablaCerosText)
+        #print(tablaCerosText)
         self.__printTabla__(2)
         for i in range(int(self.cant_restricciones) + 1):
             if i != pivote[1][0]:
@@ -275,7 +320,9 @@ class problema:
                         casilla = float(0).__format__('0.4f')
                     nuevaFila.append(float(casilla).__format__('0.4f'))
                 tablaNueva[i] = nuevaFila
-        print("------------------------------Tabla nueva - calculada ---------------------------------")
+        tablaNuevaText = "\n-------------------------Tabla nueva - calculada ----------------------------"
+        writeFile(tablaNuevaText)
+        #print(tablaNuevaText)
         self.__printTabla__(2)
         self.tablaActual = tablaNueva
         self.tablaSiguiente = []
@@ -284,21 +331,44 @@ class problema:
 
 
 
+def ejecutarSimplex(nombre_archivo):
+    removeFile()
+    datos = separarDatos(nombre_archivo)
+    problema_simplex = problema(datos)
+    problema_simplex.__agregarVariablesSimplex__()
+    problema_simplex.__makeOrdenFilas__()
+    problema_simplex.__tabularProblema__()
+    problema_simplex.__print__()
+    if problema_simplex.metodo == '1':  # Es simplex
+        tablaInicialText = "\n---------------------------Tabla inicial------------------------------------\n"
+        writeFile(tablaInicialText)
+        #print(tablaInicialText)
+        problema_simplex.__printTabla__(1)
+        metodoSimplex(problema_simplex)
+        
+
+
+def main():
+    print(len(sys.argv))
+    if len(sys.argv) <= 1:
+        print("No se han ingresado parametros ni archivos de entrada por lo que el programa se cerrara")
+        exit(0)
+    if sys.argv[1] == "-h":
+        if len(sys.argv) <= 2:
+            print("No se ha ingresado el nombre del archivo de entrada")
+            exit(0)
+        else:
+            print("Bienvenido al programa de Simplex, usted ha seleccionado la opcion " +
+            "de ayuda por lo que se le mostrara un poco de informacion \nsobre el programa!")
+            nombre_archivo = sys.argv[2]
+            ejecutarSimplex(nombre_archivo)
+    else:
+        nombre_archivo = sys.argv[1]
+        ejecutarSimplex(nombre_archivo)
+    exit(0)
 
 
 
-def main(problema):
-    problema.__agregar_variables_simplex__()
-    problema.__makeOrdenFilas__()
-    problema.__tabularProblema__()
-    problema.__print__()
-    if problema.metodo == '1':  # Es simplex
-        print("------------------------------Tabla inicial---------------------------------")
-        problema.__printTabla__(1)
-        metodoSimplex(problema)
 
 
-datos = separar_datos()
-problema = problema(datos)
-
-main(problema)
+main()
